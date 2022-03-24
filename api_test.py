5bf57efb1ae1b13fe9b71e6606ff66f7460eac54
benjaminstrandberg <73587010+benjaminstrandberg@users.noreply.github.com>
2022-03-24 12:55:45 +0100

    test

--- api_test.py
+++ api_test.py
@@ -1,8 +1,26 @@
-import requests
+import pandas
+from pandas import ExcelWriter
+from pandas import ExcelFile
 
-response_API = requests.get('http://www7.slv.se/apilivsmedel/LivsmedelService.svc/Livsmedel/Klassificering/20170101')
 
-print(response_API.status_code)
-print(response_API)
+# response_API = requests.get('http://www7.slv.se/apilivsmedel/LivsmedelService.svc/Livsmedel/Klassificering/20170101')
+
+# print(response_API.status_code)
+
+
+
+document = pandas.read_excel("livsmedelsdatabas.xlsx",sheet_name='Ra_500food')
+
+for i in document.index:
+    print(document['Name'][i])
+
+string = "chicken, haha"
+
+print(string[0:','])
+
+regex = ','
+
+
+
 
 
