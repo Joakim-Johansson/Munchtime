27b3bdc00d83e8b7f5564f6d7a685dd41303eff7
benjaminstrandberg <73587010+benjaminstrandberg@users.noreply.github.com>
2022-03-28 14:43:04 +0200

    test

--- api_test.py
+++ api_test.py
@@ -1,6 +1,8 @@
 import pandas
 from pandas import ExcelWriter
 from pandas import ExcelFile
+import re
+
 
 
 # response_API = requests.get('http://www7.slv.se/apilivsmedel/LivsmedelService.svc/Livsmedel/Klassificering/20170101')
@@ -11,14 +13,40 @@ from pandas import ExcelFile
 
 document = pandas.read_excel("livsmedelsdatabas.xlsx",sheet_name='Ra_500food')
 
+list = list()
+list.append("List of foods")
+
 for i in document.index:
-    print(document['Name'][i])
+    if(',' in document['Name'][i]):
+        temp = re.search(r"^.*?(?=\,)", document['Name'][i])[0]
+    else:
+        temp = document['Name'][i]
+    shouldAdd = True
+    for element in list:
+        if temp == element:
+            shouldAdd = False
+    if shouldAdd:
+        list.append(temp)
 
-string = "chicken, haha"
 
-print(string[0:','])
+def lookup(element, list):
+    for e in list:
+        if(e == element):
+            print("Found " + element)
+            return
+    print("Couldn't found " + element)
 
-regex = ','
+lookup('Chicken',list)
+lookup('Nocco',list)
+
+print(list)
+
+# string = "chicken, haha"
+# out = re.search(r"^.*?(?=\,)", string)[0]
+
+# print(out)
+
+# regex = ','
 
 
 
