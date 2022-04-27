15dd991317dd4a525c3bdab8e26acfde0d21bda4
Fox <fox.hamren@gmail.com>
2022-04-27 12:17:00 +0200

    First tests

--- test/RecipeCard_test.dart
+++ test/RecipeCard_test.dart
@@ -4,8 +4,17 @@ import 'package:crunchtime/views/RecipeCard.dart';
 
 void main() {
   testWidgets('RecipeCard tests', (WidgetTester tester) async {
-    // QueryDocumentSnapshot test = QueryDocumentSnapshot();
+    Map<String, dynamic> test = {
+      "name": "Carbonara",
+      "description": "Nått",
+      "ingredients": ["Bacon", "Pasta"],
+      "amount": [150, 100],
+      "nutrition": ["Fat: 24", "Carbs: 4", "Protein: 24", "Kcal: 5"],
+      "user": "Yes",
+      "img": "baconegg.jpeg",
+      "Total CO2-eq": "0.34"
+    };
 
-    // await tester.pumpWidget(RecipeCard());
+    await tester.pumpWidget(RecipeCard(test));
   });
 }
