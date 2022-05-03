c97739453d54330bb7e50825e37bf5d1baca4c87
meatloafi <71672733+meatloafi@users.noreply.github.com>
2022-05-03 13:54:06 +0200

    löste keyboard bugg

--- lib/views/ProfileEdit.dart
+++ lib/views/ProfileEdit.dart
@@ -49,94 +49,96 @@ class ProfileEditWidget extends State<ProfileEdit> {
               image: DecorationImage(
                   image: AssetImage("assets/images/hills2.png"),
                   fit: BoxFit.cover)),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Padding(
-                  padding: const EdgeInsets.fromLTRB(20, 20, 12, 0),
-                  child: Align(
-                    alignment: Alignment.topLeft,
-                    child: RichText(
-                      text: const TextSpan(
-                        children: <TextSpan>[
-                          TextSpan(
-                              text: 'About me:\n',
-                              style: TextStyle(
-                                color: Color.fromARGB(255, 27, 67, 50),
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
-                              )),
-                        ],
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Padding(
+                    padding: const EdgeInsets.fromLTRB(20, 20, 12, 0),
+                    child: Align(
+                      alignment: Alignment.topLeft,
+                      child: RichText(
+                        text: const TextSpan(
+                          children: <TextSpan>[
+                            TextSpan(
+                                text: 'About me:\n',
+                                style: TextStyle(
+                                  color: Color.fromARGB(255, 27, 67, 50),
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                )),
+                          ],
+                        ),
                       ),
-                    ),
-                  )),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(15, 0, 15, 10),
-                child: FutureBuilder(
-                    future: FirebaseFirestore.instance
-                        .collection('Users')
-                        .doc(AuthService().auth.currentUser!.uid)
-                        .get(),
-                    builder:
-                        (context, AsyncSnapshot<DocumentSnapshot> snapshot) {
-                      if (snapshot.hasData) {
-                        controller.text = snapshot.data!["bio"];
-
-                        return TextField(
-                          controller: controller,
-                          maxLines: 15,
-                          decoration: InputDecoration(
-                              border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(12),
-                          )),
-                          style: const TextStyle(
-                              color: Color.fromARGB(255, 27, 67, 50),
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold),
-                        );
-                      } else {
-                        return Container();
-                      }
-                    }),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(14, 0, 14, 6),
-                child: Align(
-                  alignment: Alignment.bottomRight,
-                  child: TextButton(
-                    onPressed: () async {
-                      bio = controller.text;
-                      await FirebaseFirestore.instance
+                    )),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(15, 0, 15, 10),
+                  child: FutureBuilder(
+                      future: FirebaseFirestore.instance
                           .collection('Users')
                           .doc(AuthService().auth.currentUser!.uid)
-                          .set({"bio": bio}, SetOptions(merge: true));
-                      setState(() {});
-
-                      // Respond to button press
-                    },
-                    style: ButtonStyle(
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(18.0),
-                            side: const BorderSide(
-                                color: Color.fromARGB(255, 27, 67, 50))),
+                          .get(),
+                      builder:
+                          (context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+                        if (snapshot.hasData) {
+                          controller.text = snapshot.data!["bio"];
+          
+                          return TextField(
+                            controller: controller,
+                            maxLines: 15,
+                            decoration: InputDecoration(
+                                border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(12),
+                            )),
+                            style: const TextStyle(
+                                color: Color.fromARGB(255, 27, 67, 50),
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold),
+                          );
+                        } else {
+                          return Container();
+                        }
+                      }),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(14, 0, 14, 6),
+                  child: Align(
+                    alignment: Alignment.bottomRight,
+                    child: TextButton(
+                      onPressed: () async {
+                        bio = controller.text;
+                        await FirebaseFirestore.instance
+                            .collection('Users')
+                            .doc(AuthService().auth.currentUser!.uid)
+                            .set({"bio": bio}, SetOptions(merge: true));
+                        setState(() {});
+          
+                        // Respond to button press
+                      },
+                      style: ButtonStyle(
+                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18.0),
+                              side: const BorderSide(
+                                  color: Color.fromARGB(255, 27, 67, 50))),
+                        ),
                       ),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.fromLTRB(10, 6, 10, 4),
-                      child: Text(
-                        "Save",
-                        style: TextStyle(
-                          color: Theme.of(context).focusColor,
-                          fontSize: 14,
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(10, 6, 10, 4),
+                        child: Text(
+                          "Save",
+                          style: TextStyle(
+                            color: Theme.of(context).focusColor,
+                            fontSize: 14,
+                          ),
                         ),
                       ),
                     ),
                   ),
-                ),
-              )
-            ],
+                )
+              ],
+            ),
           ),
         ));
   }
